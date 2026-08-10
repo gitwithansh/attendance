fa435ee2ab5d4db50acf71794af7de4ae62e6096
Ansh-Verma <anshnamdev024@gmail.com>
2026-08-11 00:11:41 +0530

    add host secure,port

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -4,6 +4,9 @@ dotenv.config();
 
 export const transporter = nodemailer.createTransport({
   service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
