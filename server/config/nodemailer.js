caa7966ad0e40f3a390da520a44043b973dff223
Ansh-Verma <anshnamdev024@gmail.com>
2026-08-11 11:29:05 +0530

    updated nodemailer

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -5,13 +5,10 @@ dns.setDefaultResultOrder('ipv4first');
 dotenv.config();
 
 export const transporter = nodemailer.createTransport({
-  // service: 'gmail',
-  host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
-  requireTLS:true,
+  service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  }
+  },
+  tls:{}
 });
