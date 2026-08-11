487d6e3b71ba4356b71b6260b66190ec37c48072
Ansh-Verma <anshnamdev024@gmail.com>
2026-08-11 10:19:27 +0530

    convert ipv6 into ipv4

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -1,12 +1,14 @@
 import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
+import dns from 'dns';
+dns.setDefaultResultOrder('ipv4first');
 dotenv.config();
 
 export const transporter = nodemailer.createTransport({
   // service: 'gmail',
   host: 'smtp.gmail.com',
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   requireTLS:true,
   auth: {
     user: process.env.EMAIL_USER,
